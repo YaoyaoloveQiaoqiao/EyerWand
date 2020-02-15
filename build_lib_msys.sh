dd659e459f2fedc5a8d0833ffed91e2232260747
Redknot <redknotmiaoyuqiao@gmail.com>
2020-02-16 04:49:45 +0800

    windows build

---
+++ build_lib_msys.sh
@@ -0,0 +1,23 @@
+cd Lib
+rm -rf EyerLib
+cd ../
+
+cd EyerLib
+cd Lib
+mkdir build_a
+
+rm -rf install
+
+cd build_a
+
+cmake -G"Unix Makefiles" ../
+make -j4
+make install
+
+cd ..
+rm -rf build_a
+
+cd ..
+cd ..
+
+cp -r EyerLib/Lib/install/ Lib/EyerLib
--- init_msys.sh
+++ init_msys.sh
@@ -1,3 +1,5 @@
+export PATH=/c/Cmake/bin:$PATH
+
 if [ -d ./Eyer3rdpart ];then 
     rm -rf Eyer3rdpart
 fi
@@ -39,8 +41,24 @@ make clean
 make -j4
 make install
 
-cd ${basepath}
+cd ${basepath}/Eyer3rdpart/glfw-3.3.2
+mkdir build
+cd build
+cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=../glfw_install ../
+make clean
+make -j4
+make install
 
+cd ${basepath}/Eyer3rdpart/libyuv
+mkdir build
+cd build
+cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=../libyuv_install ../
+make clean
+make -j4
+make install
+
+
+cd ${basepath}
 
 if [ -d ./Lib ];then
     rm -rf Lib
@@ -53,6 +71,8 @@ cp -r Eyer3rdpart/ffmpeg_3.2.14/ffmpeg_install Lib/ffmpeg_install
 cp -r Eyer3rdpart/freetype-2.10.0/freetype_install Lib/freetype_install
 cp -r Eyer3rdpart/libpng-1.6.37/libpng_install Lib/libpng_install
 
+cp -r Eyer3rdpart/glfw-3.3.2/glfw_install Lib/glfw_install
+cp -r Eyer3rdpart/libyuv/libyuv_install Lib/libyuv_install
 
 
 
