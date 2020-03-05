7e5e83f11ec7ddbad98d069234657651d92ad671
Redknot <redknotmiaoyuqiao@gmail.com>
2020-03-06 05:09:32 +0800

    csharp project

---
+++ EyerWandSharp/EyerWandSharpTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EyerWandSharp;
+
+namespace EyerWandSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EyerWand.getVersion());
+
+            EyerWandBuilder eyerWandBuilder = new EyerWandBuilder("./test.mp4", 1280, 720, 30);
+
+            EyerWandVideoTrack videoTrack = new EyerWandVideoTrack();
+
+
+
+
+
+            eyerWandBuilder.addVideoTrack(videoTrack);
+
+            eyerWandBuilder.uninit();
+        }
+    }
+}
