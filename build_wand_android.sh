8fb5188cdf24ffa8a53fe89048accdd3d61921ed
redknot <redknotmiaoyuqiao@gmail.com>
2020-05-02 16:27:18 +0800

    build android

--- build_wand_android.sh
+++ build_wand_android.sh
@@ -26,14 +26,49 @@ rm -rf build_a
 cd ..
 cd ..
 
-cp -r EyerVideoWand/EyerVideoWand/install/ Lib/EyerVideoWand
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "================Gen so libEyerWandJni.so================"
+cd ${basepath}/EyerVideoWand/EyerVideoWand/install/lib
+ls -lh
+
+
+cd ${basepath}/Lib/
+if [ -d ./EyerVideoWand ];then 
+    rm -rf EyerVideoWand
+fi
+
+cp -r ${basepath}/EyerVideoWand/EyerVideoWand/install/ ${basepath}/Lib/EyerVideoWand
+
+cd ${basepath}/Lib/EyerVideoWand/lib
+ls -lh
+
+echo "================Gen so libEyerWandJni.so================"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
 
 echo "================Update so to EyerWandEditor================"
 
 cd ${basepath}/EyerWandEditor/EyerWandEditorAndroid/eyer_wand_editor_lib/src/main/jniLibs
-rm -r armeabi-v7a
+
+if [ -d ./armeabi-v7a ];then 
+    rm -rf armeabi-v7a
+fi
 mkdir armeabi-v7a
+cd armeabi-v7a
+
+
 
 cd ${basepath}
 
 cp ${basepath}/Lib/EyerVideoWand/lib/libEyerWandJni.so ${basepath}/EyerWandEditor/EyerWandEditorAndroid/eyer_wand_editor_lib/src/main/jniLibs/armeabi-v7a/libEyerWandJni.so
+
+cd ${basepath}/EyerWandEditor/EyerWandEditorAndroid/eyer_wand_editor_lib/src/main/jniLibs/armeabi-v7a
+ls -lh
